fa20876904a8595bdd4a0f297fc7775c8408215f
schnaya <76622822+schnaya@users.noreply.github.com>
2022-05-13 18:38:53 +0300

    Add files via upload

---
+++ acc.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+$(document).ready(function () {
+    //прикрепляем клик по заголовкам acc-head
+    $('#accordeon .acc-head').on('click', f_acc);
+});
+
+function f_acc() {
+    //скрываем все кроме того, что должны открыть
+    $('#accordeon .acc-body').not($(this).next()).slideUp(1000);
+    // открываем или скрываем блок под заголовком, по которому кликнули
+    $(this).next().slideToggle(2000);
+}
